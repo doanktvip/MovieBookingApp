ccc9bbc8c50791b7fa84aecde7d369cbc4e70f27
Ho Dat <hovandat2005@gmail.com>
2026-03-20 16:14:31 +0700

    update header and carousel to home feature

--- movieapp/index.py
+++ movieapp/index.py
@@ -1,9 +1,14 @@
-from movieapp import app
+from movieapp import app, dao
 from flask import Flask, render_template, request, url_for
 from flask_login import login_user,current_user, logout_user
 
+#Trang chủ
 @app.route('/')
 def index():
-    return render_template('index.html')
+    movies=dao.load_movies()
+    genres=dao.load_genres()
+    return render_template('index.html',movies=movies,genres=genres)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
