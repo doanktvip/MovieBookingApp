add87c22ff71e76bacfd0aaea92a622d5a5948a8
Ho Dat <hovandat2005@gmail.com>
2026-04-13 12:09:13 +0700

    update search and pagination to checkin

--- movieapp/__init__.py
+++ movieapp/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.secret_key = "nguyenvandoansieudeptrai"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/moviedb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:hod2t123@localhost/moviedb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 4
 app.config["HOLD_TIME_MINUTES"] = 10
